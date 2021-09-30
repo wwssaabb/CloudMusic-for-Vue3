3280e7b62107ba2cfcf0d84470b768ee4fc9cd3e
wwssaabb <919507905@qq.com>
2021-10-01 03:00:22 +0800

    更新dj主播页面

---
+++ src/components/DjRadio/browseList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="borwse-list">
+    <Title :title="title">
+      <template #right>
+        <span class="more td_u">更多 ></span>
+      </template>
+    </Title>
+    <div class="list">
+      <div class="item" v-for="item in list" :key="item.id">
+        <img src="item.pictrue" alt="">
+        <div class="right">
+          <div class="name">{{}}</div>
+          <div class="desc">{{}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Title from '../Title.vue'
+
+const props=defineProps({
+  title:{
+    type:String,
+    required:true
+  },
+  list:{
+    type:[],
+    required:true
+  }
+})
+
+</script>
+
+<style scoped>
+
+</style>
