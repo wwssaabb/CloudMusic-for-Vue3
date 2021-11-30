d3827a7a4b2d3f4db8c494abad403f1b539a55e7
wwssaabb <919507905@qq.com>
2021-11-30 15:58:01 +0800

    update deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 /*
  * @Author: wwssaabb
  * @Date: 2021-09-17 11:23:13
- * @LastEditTime: 2021-11-30 15:36:02
+ * @LastEditTime: 2021-11-30 15:57:12
  * @FilePath: \CloudMusic-for-Vue3\vite.config.ts
  */
 import vue from "@vitejs/plugin-vue";
 
 module.exports = {
   plugins: [vue()],
-  base: "/CloudMusic-for-Vue3/",
+  base: "/cloudmusic-for-vue3/",
   outDir: "dist",
   port: 3000,
   open: true,
